7f646bf2912d560506cd7131c2364a973236b0f5
Tayyeb <tayyeb@tsaze.com>
2016-03-16 01:59:24 +0330

    use MANIFEST.in and cleanup setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import sys
 
 from setuptools import setup, find_packages
 
-version = __import__('jquery').__version__
 
+version = __import__('jquery').__version__
 
 if sys.argv[-1] == 'publish':
     os.system('git tag -a %s -m "version %s"' % (version, version))
@@ -17,18 +17,18 @@ if sys.argv[-1] == 'publish':
 
 
 setup(
-    name			='static-jquery',
-    version			=version,
-    url				="https://github.com/wtayyeb/static-jquery",
-    description		='jQuery packaged in an handy django app to speed up new applications and deployment.',
-    author			='wtayyeb',
-    author_email	='wtayyeb@gmail.com',
-    license			='MIT',
-    keywords		='django jquery staticfiles templatetags',
-    packages		=find_packages(),
-    package_data	={'jquery': ['static/jquery/*.js', ]},
-	install_requires=['django-appconf', ],
-    classifiers		=[
+    name='static-jquery',
+    version=version,
+    url="https://github.com/wtayyeb/static-jquery",
+    description='jQuery packaged in an handy django app to speed up new applications and deployment.',
+    author='wtayyeb',
+    author_email='wtayyeb@gmail.com',
+    license='MIT',
+    keywords='django jquery staticfiles templatetags',
+    packages=find_packages(),
+    install_requires=['django-appconf', ],
+    zip_safe=False,
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
